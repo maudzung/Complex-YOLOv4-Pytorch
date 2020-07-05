745bce7f46cfa122eb646e48c271275787f92e8e
maudzung <nguyenmaudung93.kstn@gmail.com>
2020-07-05 10:54:14 +0900

    Test: Add tests for dataset

--- src/data_process/kitti_dataloader.py
+++ src/data_process/kitti_dataloader.py
@@ -56,9 +56,36 @@ def create_test_dataloader(configs):
 
 
 if __name__ == '__main__':
+    import cv2
+    import numpy as np
+
     from config.config import parse_configs
+    import data_process.kitti_bev_utils as bev_utils
 
     configs = parse_configs()
     configs.distributed = False  # For testing
     train_dataloader, val_dataloader, train_sampler = create_train_val_dataloader(configs)
     print('len train_dataloader: {}, val_dataloader: {}'.format(len(train_dataloader), len(val_dataloader)))
+
+    img_size = configs.BEV_WIDTH
+
+    for batch_i, (_, imgs, targets) in enumerate(val_dataloader):
+
+        # Rescale target
+        targets[:, 2:6] *= img_size
+        # Get yaw angle
+        targets[:, 6] = torch.atan2(targets[:, 6], targets[:, 7])
+
+        img = imgs.squeeze() * 255
+        img = img.permute(1, 2, 0).numpy().astype(np.uint8)
+        img_display = np.zeros((img_size, img_size, 3), np.uint8)
+        img_display[...] = img[...]
+
+        for c, x, y, w, l, yaw in targets[:, 1:7].numpy():
+            # Draw rotated box
+            bev_utils.drawRotatedBox(img_display, x, y, w, l, yaw, configs.colors[int(c)])
+
+        cv2.imshow('img-kitti-bev', img_display)
+
+        if cv2.waitKey(0) & 0xff == 27:
+            break
